1b7ba472f6ba82db2b23ea53ec2930b81e8d5481
Sirriisha <gantasaisirriisha@gmail.com>
2024-05-15 15:50:51 +0530

    done

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+// Nav hamburgerburger selections
+const menubar = document.querySelector("#menu-bar");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+// Scroll to top selection
+const scrollUp = document.querySelector("#scroll-up");
+
+// Select nav links
+const navLink = document.querySelectorAll(".nav-link");
+
+// Hamburger menu function
+menubar.addEventListener("click", () => {
+  ul.classList.toggle("show");
+});
+
+// Close hamburger menu when a link is clicked
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+  })
+);
+
+// scroll to top functionality
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
